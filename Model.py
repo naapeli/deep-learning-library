59167e8acdf8b6ca91a432cbc8e5349edc0e220e
naapeli <130310206+naapeli@users.noreply.github.com>
2024-04-14 22:33:44 +0300

    Separated testing logic from the Model.py file

--- Model.py
+++ Model.py
@@ -1,9 +1,6 @@
-from Layers.Dense import Dense
-from Layers.Activations.Tanh import Tanh
 from Layers.Input import Input
 from Losses.MSE import mse
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class Model:
@@ -55,35 +52,3 @@ class Model:
             errors[epoch] = error
             if epoch % loss_step == 0: print(f"Epoch: {epoch} - Error: {error}")
         return errors
-
-
-x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([0, 1, 1, 0])
-
-model = Model(2)
-model.add(Dense(3, activation=Tanh()))
-model.add(Dense(1, activation=Tanh()))
-model.compile(optimiser=None, loss=mse())
-model.summary()
-errors = model.fit(x, y, epochs=1000, loss_step=100)
-plt.plot(errors)
-plt.xlabel("Epochs")
-plt.ylabel("Mean squared error")
-plt.show()
-print(model.predict(np.array([0, 0])))
-print(model.predict(np.array([0, 1])))
-print(model.predict(np.array([1, 0])))
-print(model.predict(np.array([1, 1])))
-print(model.predict(np.array([0.5, 0.5])))
-
-Xv, Yv = np.meshgrid(np.linspace(0, 1), np.linspace(0, 1))
-values = []
-for x, y in zip(Xv.flatten(), Yv.flatten()):
-    values.append(model.predict(np.array([x, y])))
-z = np.array(values)
-z = z.reshape(Xv.shape)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-surf = ax.scatter(Xv, Yv, z)
-fig.colorbar(surf)
-plt.show()
---
+++ testing.py
@@ -0,0 +1,38 @@
+from Model import Model
+from Layers.Dense import Dense
+from Layers.Activations.Tanh import Tanh
+from Losses.MSE import mse
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 0])
+
+model = Model(2)
+model.add(Dense(3, activation=Tanh()))
+model.add(Dense(1, activation=Tanh()))
+model.compile(optimiser=None, loss=mse())
+model.summary()
+errors = model.fit(x, y, epochs=1000, loss_step=100)
+plt.plot(errors)
+plt.xlabel("Epochs")
+plt.ylabel("Mean squared error")
+plt.show()
+print(model.predict(np.array([0, 0])))
+print(model.predict(np.array([0, 1])))
+print(model.predict(np.array([1, 0])))
+print(model.predict(np.array([1, 1])))
+print(model.predict(np.array([0.5, 0.5])))
+
+Xv, Yv = np.meshgrid(np.linspace(0, 1), np.linspace(0, 1))
+values = []
+for x, y in zip(Xv.flatten(), Yv.flatten()):
+    values.append(model.predict(np.array([x, y])))
+z = np.array(values)
+z = z.reshape(Xv.shape)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.scatter(Xv, Yv, z)
+fig.colorbar(surf)
+plt.show()
