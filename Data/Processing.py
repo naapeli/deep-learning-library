f47b66340862f6c4ba5e0114db1e2438320b7658
naapeli <aatu.selkee@gmail.com>
2024-04-19 23:14:55 +0300

    implemented one_hot_decode and modified data_split to allow for test split

--- Data/Processing.py
+++ Data/Processing.py
@@ -3,35 +3,44 @@ from math import floor
 
 
 """
-Splits the data into train and validation sets
+Splits the data into train, validation and test sets
 
 X.shape = (data_length, input_shape)
 Y.shape = (data_length, output_shape)
 """
-def validation_split(X, Y, split=0.2):
+def data_split(X, Y, train_split=0.8, validation_split=0.2):
+    assert train_split + validation_split <= 1 and validation_split >= 0 and train_split >= 0, "Splits must be between 0 and 1 and their sum less than or equal to 1."
     axis = 0
     data_length = X.size(axis)
     perm = torch.randperm(data_length, requires_grad=False)
     x_data = X.index_select(axis, perm)
     y_data = Y.index_select(axis, perm)
-    split_index = floor(data_length * split)
-    x_train, y_train = x_data[split_index:], y_data[split_index:]
-    x_val, y_val = x_data[:split_index], y_data[:split_index]
-    return x_train, y_train, x_val, y_val
+    split_index1 = floor(data_length * train_split)
+    split_index2 = floor(data_length * (train_split + validation_split))
+    x_train, y_train = x_data[:split_index1], y_data[:split_index1]
+    x_val, y_val = x_data[split_index1:split_index2], y_data[split_index1:split_index2]
+    x_test, y_test = x_data[split_index2:], y_data[split_index2:]
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
 """
 one-hot encodes the given categorical Y labels
 
 Y.shape = (data_length, 1)
 """
-def one_hot_encode(Y):
-    assert (len(Y.shape) == 2 and Y.shape[1] == 1) or len(Y.shape) == 1, "Y-labels must be of shape (data_length, 1)"
-    unique_elements = torch.unique(Y)
-    element_to_index = {element.item(): i for i, element in enumerate(unique_elements)}
-    one_hot_length = tuple(unique_elements.size())[0]
-    label_to_distribution = torch.tensor([_get_distribution(element_to_index[y.item()], one_hot_length) for y in Y], requires_grad=False)
-    return label_to_distribution
+class OneHotEncoder:
+    def one_hot_encode(self, Y):
+        assert (len(Y.shape) == 2 and Y.shape[1] == 1) or len(Y.shape) == 1, "Y-labels must be of shape (data_length, 1)"
+        unique_elements = torch.unique(Y)
+        self.element_to_index = {element.item(): i for i, element in enumerate(unique_elements)}
+        self.index_to_element = {i: element for element, i in self.element_to_index.items()}
+        one_hot_length = tuple(unique_elements.size())[0]
+        label_to_distribution = torch.tensor([self._get_distribution(self.element_to_index[y.item()], one_hot_length) for y in Y], requires_grad=False)
+        return label_to_distribution
+    
+    def one_hot_decode(self, data):
+        assert len(data.shape) == 2, "Input must be of shape (data_length, number_of_categories)"
+        return torch.tensor([self.index_to_element[torch.nonzero(tensor).item()] for tensor in data], requires_grad=False)
 
-def _get_distribution(index, size):
-    distribution = [0 if i != index else 1 for i in range(size)]
-    return distribution
+    def _get_distribution(self, index, size):
+        distribution = [0 if i != index else 1 for i in range(size)]
+        return distribution
