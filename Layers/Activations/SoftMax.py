7f1880e3c34eda626314390c85faa21375109276
naapeli <aatu.selkee@gmail.com>
2024-04-21 20:47:16 +0300

    Debugged softmax to work with differing batch_sizes

--- Layers/Activations/SoftMax.py
+++ Layers/Activations/SoftMax.py
@@ -9,12 +9,11 @@ class SoftMax(Activation):
 
     def forward(self, input, **kwargs):
         self.input = input
-        exponential_input = torch.exp(self.input)
-        self.output = exponential_input / torch.sum(exponential_input)
+        exponential_input = torch.exp(self.input - torch.max(self.input, dim=1, keepdim=True).values)
+        self.output = exponential_input / torch.sum(exponential_input, dim=1, keepdim=True)
         return self.output
     
     def backward(self, dCdy, **kwargs):
-        n = self.output.shape[0]
-        output_repeated = torch.tile(self.output, (n, 1))
-        dCdx = (output_repeated.T * (torch.eye(n) - output_repeated)) @ dCdy
+        n = self.output_size
+        dCdx = torch.stack([(dCdy[i] @ (torch.tile(datapoint, (n, 1)).T * (torch.eye(n) - torch.tile(datapoint, (n, 1))))) for i, datapoint in enumerate(self.output)])
         return dCdx
