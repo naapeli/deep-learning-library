d89899d51491024bd44a395b36a0f01248640b60
naapeli <aatu.selkee@gmail.com>
2024-04-22 14:03:10 +0300

    Modified accuracy to work on binary classification problems as well

--- Data/Metrics.py
+++ Data/Metrics.py
@@ -2,7 +2,14 @@ import torch
 
 
 def accuracy(predictions, true_output):
-    prediction_indicies = torch.argmax(predictions, dim=1)
-    correct_indicies = torch.argmax(true_output, dim=1)
-    correct = prediction_indicies == correct_indicies
-    return torch.sum(correct).item() / correct.shape[0]
+    # multi-class accuracy
+    if len(predictions.shape) == 2 and predictions.shape[1] > 1:
+        prediction_indicies = torch.argmax(predictions, dim=1)
+        correct_indicies = torch.argmax(true_output, dim=1)
+        correct = prediction_indicies == correct_indicies
+        return torch.sum(correct).item() / correct.shape[0]
+    # 2-class accuracy
+    else:
+        prediction_values = predictions >= 0.5
+        correct = prediction_values == true_output
+        return torch.sum(correct).item() / correct.shape[0]
