ef4262ba54cfb302f29519b11cc782c4a160da9c
naapeli <aatu.selkee@gmail.com>
2024-05-27 14:34:22 +0300

    Fixed a bug in binary cross entropy gradient

--- DeepLearning/Losses/BCE.py
+++ DeepLearning/Losses/BCE.py
@@ -6,7 +6,7 @@ Calculates the binary cross entropy and the gradient between a prediction vector
 """
 class bce:
     def loss(self, prediction, true_output):
-        return -torch.mean(true_output.reshape(prediction.shape) * torch.log2(prediction + 1e-5) - (1 - true_output.reshape(prediction.shape)) * torch.log2(1 - prediction + 1e-5))
+        return -torch.mean(true_output.reshape(prediction.shape) * torch.log2(prediction + 1e-10) - (1 - true_output.reshape(prediction.shape)) * torch.log2(1 - prediction + 1e-10))
 
     def gradient(self, prediction, true_output):
-        return -true_output.reshape(prediction.shape) / (prediction + 1e-5)
+        return (prediction - true_output) / (prediction * (1 - prediction) + 1e-10)
