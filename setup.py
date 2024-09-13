30a75d6246c16cc8e718f8f2e3372c2f50cc775b
naapeli <aatu.selkee@gmail.com>
2024-09-13 16:12:47 +0300

    Added a setup file to the project

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='DLL',
+    version='1.0',
+    author='Aatu Selkee',
+    author_email='aatu.selkee@gmail.com',
+    description='A basic deep and machine learning library. Made for learning purposes and is not supposed to be used for true applications.',
+    # long_description='A detailed description of the project',
+    # long_description_content_type='text/markdown',
+    url='https://github.com/naapeli/deep-learning-library',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'torch',
+        'tensorflow',
+        "matplotlib",
+        "numpy",
+        "sklearn",
+        "scienceplots",
+        "scipy"
+    ],
+    # classifiers=[
+    #     'Programming Language :: Python :: 3',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent',
+    # ],
+    # python_requires='>=3.6',
+)
