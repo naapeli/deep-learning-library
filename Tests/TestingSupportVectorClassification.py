a3689d7e9d5e31573c082456580d8b10068914d0
naapeli <aatu.selkee@gmail.com>
2024-09-20 23:48:22 +0300

    Support vector classification working with the implemented kernels. Still need to implement the one vs one fitting method

--- Tests/TestingSupportVectorClassification.py
+++ Tests/TestingSupportVectorClassification.py
@@ -1,4 +1,5 @@
 from src.DLL.MachineLearning.SupportVectorMachines.SVC import SVC
+from src.DLL.MachineLearning import Kernels
 from src.DLL.Data.Preprocessing import data_split
 from src.DLL.Data.Metrics import accuracy
 
@@ -7,50 +8,30 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 
 
-X, y = datasets.make_blobs(n_samples=100, n_features=2, centers=2, cluster_std=4)
+X, y = datasets.make_blobs(n_samples=1000, n_features=2, cluster_std=2.5, centers=4)
 
-plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=5)
 plt.show()
 
 X = torch.from_numpy(X)
-y = torch.from_numpy(y) * 2 - 1
+y = torch.from_numpy(y)
 X_train, y_train, X_test, y_test, _, _ = data_split(X, y)
 
-model = SVC()
-model.fit(X_train, y_train, epochs=200)
+model = SVC(kernel=Kernels.LinearCovariance())
+model.fit(X_train, y_train)
 predictions = model.predict(X_test)
-print(accuracy(predictions, y_test))
+print(round(accuracy(predictions, y_test), 3))
 
-# From AssemblyAI
-def visualize_svm():
-    def get_hyperplane_value(x, w, b, offset):
-        return (-w[0] * x + b + offset) / w[1]
+n = 500
+x_min, X_max = X[:, 0].min(), X[:, 0].max()
+y_min, y_max = X[:, 1].min(), X[:, 1].max()
+x_grid = torch.linspace(x_min, X_max, n)
+y_grid = torch.linspace(y_min, y_max, n)
+Xv, Yv = torch.meshgrid(x_grid, y_grid, indexing="ij")
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    # plt.scatter(X_train[:, 0], X_train[:, 1], marker="o", c=y_train)
-    plt.scatter(X_test[:, 0], X_test[:, 1], marker="x", c=(y_test == predictions))
+X_grid = torch.stack((Xv.flatten(), Yv.flatten()), dim=1).to(X.dtype)
+grid_predictions = model.predict(X_grid).reshape((n, n))
 
-    x0_1 = torch.amin(X[:, 0])
-    x0_2 = torch.amax(X[:, 0])
-
-    x1_1 = get_hyperplane_value(x0_1, model.weight, model.bias, 0)
-    x1_2 = get_hyperplane_value(x0_2, model.weight, model.bias, 0)
-
-    x1_1_m = get_hyperplane_value(x0_1, model.weight, model.bias, -1)
-    x1_2_m = get_hyperplane_value(x0_2, model.weight, model.bias, -1)
-
-    x1_1_p = get_hyperplane_value(x0_1, model.weight, model.bias, 1)
-    x1_2_p = get_hyperplane_value(x0_2, model.weight, model.bias, 1)
-
-    ax.plot([x0_1, x0_2], [x1_1, x1_2], "y--")
-    ax.plot([x0_1, x0_2], [x1_1_m, x1_2_m], "k")
-    ax.plot([x0_1, x0_2], [x1_1_p, x1_2_p], "k")
-
-    x1_min = torch.amin(X[:, 1])
-    x1_max = torch.amax(X[:, 1])
-    ax.set_ylim([x1_min - 3, x1_max + 3])
-
-    plt.show()
-
-visualize_svm()
+plt.contourf(Xv.numpy(), Yv.numpy(), grid_predictions.numpy(), alpha=0.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=5)
+plt.show()
--- src/DLL/MachineLearning/SupportVectorMachines/SVC.py
+++ src/DLL/MachineLearning/SupportVectorMachines/SVC.py
@@ -1,64 +1,88 @@
 import torch
+import cvxopt
+import numpy as np
+import copy
 
 from ..Kernels import SquaredExponentialCovariance
 from ...DeepLearning.Optimisers.ADAM import Adam
 
 
 class SVC:
-    def __init__(self, kernel=SquaredExponentialCovariance(), C=1, learning_rate=0.001, device=torch.device("cpu")):
+    def __init__(self, kernel=SquaredExponentialCovariance(), C=1, learning_rate=0.001):
         self.kernel = kernel
         self.C = C
         self.learning_rate = learning_rate
-        self.device = device
     
     def _kernel_matrix(self, X1, X2):
-        covariance = torch.tensor([[self.kernel(x1, x2) for x1 in X1] for x2 in X2], dtype=X1.dtype, device=self.device).T
-        return covariance
+        return self.kernel(X1, X2).to(X1.dtype)
     
-    def fit(self, X, y, epochs=100, optimiser=None):
+    def fit(self, X, y, multi_method="ovr"):
         if len(torch.unique(y)) > 2:
             self.multiclass = True
-            return self._multi_fit(X, y, epochs=epochs, optimiser=optimiser)
+            return self._multi_fit(X, y, multi_method)
         self.multiclass = False
         if set(torch.unique(y)) == {0, 1}: y[y == 0] = -1
-        self.y = torch.where(y <= 0, -1, 1).to(X.dtype)
+        self.y = torch.where(y <= 0, -1, 1).reshape((-1, 1)).to(X.dtype)
         self.transform_y = not torch.all(self.y == y)
         self.X = X
+        n = X.shape[0]
+        K = self._kernel_matrix(X, X)
 
-        self.weight = torch.randn(X.shape[1], dtype=X.dtype)
-        self.bias = torch.zeros(1, dtype=X.dtype)
+        P = cvxopt.matrix((self.y @ self.y.T * K).numpy())
+        q = cvxopt.matrix(-np.ones((n, 1)))
+        A = cvxopt.matrix((self.y.T).numpy())
+        b = cvxopt.matrix(np.zeros(1))
+        G = cvxopt.matrix(np.vstack((-np.eye(n), np.eye(n))))
+        h = cvxopt.matrix(np.vstack((np.zeros((n, 1)), np.ones((n, 1)) * self.C)))
+        
+        cvxopt.solvers.options['show_progress'] = False
+        sol = cvxopt.solvers.qp(P, q, G, h, A, b)
+        self.alpha = torch.tensor(np.array(sol["x"]), )
 
-        optimiser = Adam(self.learning_rate) if optimiser is None else optimiser
-        optimiser.initialise_parameters([self.weight, self.bias])
-        # self.K = self._kernel_matrix(X, X)
+        self.is_sv = ((self.alpha - 1e-3 > 0) & (self.alpha <= self.C)).squeeze()
+        self.margin_sv = torch.argmax(((0 < self.alpha - 1e-3) & (self.alpha < self.C - 1e-3)).to(torch.int32))
 
-        for epoch in range(epochs):
-            hinge_loss = 0
-            for x_i, y_i in zip(self.X, self.y):
-                # gradient of the hinge loss with l2 regularisation
-                comparison = (1 - y_i * (x_i @ self.weight - self.bias)).item()
-                if 0 >= comparison:
-                    self.weight.grad = self.learning_rate * (2 * self.weight)
-                    self.bias.grad = torch.zeros_like(self.bias)
-                else:
-                    hinge_loss += comparison
-                    self.weight.grad = self.learning_rate * (2 * self.weight - self.C * x_i * y_i)
-                    self.bias.grad = torch.tensor([self.learning_rate * y_i])
-            
-                # update the parameters
-                optimiser.update_parameters()
-            hinge_loss *= self.C / self.X.shape[1]
-            hinge_loss += (self.weight ** 2).sum().item()
-            print(f"Epoch: {epoch + 1} - Hinge loss: {round(hinge_loss, 2)}")
+    def _multi_fit(self, X, y, method):
+        assert hasattr(self, "multiclass"), "Use SVC.fit(X, y) rather than SVC._multi_fit(X, y) even if your data is multidimensional."
+        self.method = method
+        if method == "ovr":
+            self.n_classes = len(torch.unique(y))
+            self.classes = torch.unique(y)
+            self.classifiers = []
+            for label in self.classes:
+                Xs, ys = X, copy.deepcopy(y)
+                ys[ys != label], ys[ys == label] = -1, +1
+                classifier = SVC(kernel=self.kernel, C=self.C, learning_rate=self.learning_rate)
+                classifier.fit(Xs, ys)
+                self.classifiers.append(classifier)
 
-    def _multi_fit(X, y, epochs=100):
-        pass
+        elif method == "ovo":
+            raise NotImplementedError()
+        else:
+            raise NotImplementedError()
 
-    def predict(self, X):
+    def predict(self, X, _from_multi=False):
         assert hasattr(self, "multiclass"), "SVC.fit(X, y) must be called before predicting."
-        if self.multiclass: return self._multi_predict(X)
-        prediction = X @ self.weight - self.bias
-        return ((torch.sign(prediction) + 1) / 2).to(torch.int32) if self.transform_y else torch.sign(prediction).to(torch.int32)
+        if self.multiclass:
+            self.flag = None
+            return self._multi_predict(X)
+        x_s, y_s = self.X[self.margin_sv, torch.newaxis], self.y[self.margin_sv]
+        alpha_sv, y_sv, X_sv = self.alpha[self.is_sv], self.y[self.is_sv], self.X[self.is_sv]
+        bias = y_s - torch.sum(alpha_sv * y_sv * self._kernel_matrix(X_sv, x_s), dim=0)
+        score = torch.sum(alpha_sv * y_sv * self._kernel_matrix(X_sv, X), dim=0) + bias
+        if _from_multi:
+            return score
+        return ((score + 1) / 2).to(torch.int32) if self.transform_y else score.to(torch.int32)
 
     def _multi_predict(self, X):
-        raise NotImplementedError()
+        assert hasattr(self, "flag"), "Use SVC.predict(X, y) rather than SVC._multi_predict(X, y) even if your data is multidimensional."
+        if self.method == "ovr":
+            predictions = torch.zeros((X.shape[0], self.n_classes))
+            for i, classifier in enumerate(self.classifiers):
+                predictions[:, i] = classifier.predict(X, _from_multi=True)
+            return self.classes[torch.argmax(predictions, dim=1)]
+
+        elif self.method == "ovo":
+            raise NotImplementedError()
+        else:
+            raise NotImplementedError()
