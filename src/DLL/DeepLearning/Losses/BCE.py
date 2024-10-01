1859d2fa5be0ce4bba6a2c4629c397402e0dff10
naapeli <aatu.selkee@gmail.com>
2024-10-01 11:36:45 +0300

    Changed the base of the logarithm in cross entropy losses

--- src/DLL/DeepLearning/Losses/BCE.py
+++ src/DLL/DeepLearning/Losses/BCE.py
@@ -6,7 +6,7 @@ Calculates the binary cross entropy and the gradient between a prediction vector
 """
 class bce:
     def loss(self, prediction, true_output):
-        return -torch.mean(true_output.reshape(prediction.shape) * torch.log2(prediction + 1e-10) + (1 - true_output.reshape(prediction.shape)) * torch.log2(1 - prediction + 1e-10))
+        return -torch.mean(true_output.reshape(prediction.shape) * torch.log(prediction + 1e-10) + (1 - true_output.reshape(prediction.shape)) * torch.log(1 - prediction + 1e-10))
 
     def gradient(self, prediction, true_output):
         return (prediction - true_output) / (prediction * (1 - prediction) + 1e-10)
--- src/DLL/DeepLearning/Losses/CCE.py
+++ src/DLL/DeepLearning/Losses/CCE.py
@@ -6,7 +6,7 @@ Calculates the categorical cross entropy and the gradient between a prediction v
 """
 class cce:
     def loss(self, prediction, true_output):
-        return -torch.mean(true_output.reshape(prediction.shape) * torch.log2(prediction + 1e-5))
+        return -torch.mean(true_output.reshape(prediction.shape) * torch.log(prediction + 1e-5))
 
     def gradient(self, prediction, true_output):
         return -true_output.reshape(prediction.shape) / (prediction + 1e-5)
