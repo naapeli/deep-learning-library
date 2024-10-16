836bab52d850063ee1ffbc3a0cb23a1d82e7fa26
naapeli <aatu.selkee@gmail.com>
2024-10-16 15:03:18 +0300

    Implemented gradient boosting machines and fixed a bug in regression tree variance calculations

---
+++ Tests/TestingGradientBoostingClassifier.py
@@ -0,0 +1,33 @@
+import torch
+from sklearn import datasets
+from sklearn.metrics import auc as sk_auc
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.DLL.MachineLearning.SupervisedLearning.RandomForests import GradientBoostingClassifier
+from src.DLL.Data.Preprocessing import data_split
+from src.DLL.Data.Metrics import accuracy, roc_curve, auc
+
+
+np.random.seed(0)
+X, y = datasets.make_blobs(n_features=2, n_samples=1000, centers=2)
+plt.scatter(X[:, 0], X[:, 1], c=y)
+
+x_train, y_train, _, _, x_test, y_test = data_split(torch.from_numpy(X).to(dtype=torch.float32), torch.from_numpy(y), train_split=0.7, validation_split=0.0)
+
+model = GradientBoostingClassifier(n_trees=10, learning_rate=0.5)
+model.fit(x_train, y_train)
+y_pred = model.predict_proba(x_test)
+print(model.predict(x_test)[:10], y_test[:10])
+print(accuracy(y_pred, y_test))
+
+thresholds = torch.linspace(0, 1, 100)
+fpr, tpr = roc_curve(y_pred, y_test, thresholds)
+plt.figure()
+plt.title(f"ROC curve with auc = {auc(fpr, tpr)}")
+print(sk_auc(fpr, tpr))
+plt.plot([0, 1], [0, 1])
+plt.plot(fpr, tpr)
+plt.xlabel("False positive rate")
+plt.ylabel("True positive rate")
+plt.show()
