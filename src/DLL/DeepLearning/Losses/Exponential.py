9adac8f3cadceb558495fbe424c2f1ca1699ca79
naapeli <aatu.selkee@gmail.com>
2024-10-24 22:33:20 +0300

    gradient boosting classifier working for multi class classification as well

--- src/DLL/DeepLearning/Losses/Exponential.py
+++ src/DLL/DeepLearning/Losses/Exponential.py
@@ -3,19 +3,16 @@ import torch
 
 """
 Calculates the exponential loss and the gradient between a prediction vector as well as intended outputs. Meant for binary classification.
+https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/_loss/loss.py#L268 - ExponentialLoss
 """
 class exponential:
     def loss(self, prediction, true_output):
-        # assert set(torch.unique(true_output).numpy()) == {-1, 1}, "The classes must be labelled -1 and 1."
         assert set(torch.unique(true_output).numpy()) == {0, 1}, "The classes must be labelled 0 and 1."
-        # return torch.exp(-true_output * prediction)
         true_output = true_output.float()
         loss = true_output * torch.exp(-prediction) + (1 - true_output) * torch.exp(prediction)
         return torch.mean(loss)
 
     def gradient(self, prediction, true_output):
-        # assert set(torch.unique(true_output).numpy()) == {-1, 1}, "The classes must be labelled -1 and 1."
         assert set(torch.unique(true_output).numpy()) == {0, 1}, "The classes must be labelled 0 and 1."
-        # return -true_output * self.loss(prediction, true_output)
         true_output = true_output.float()
         return -true_output * torch.exp(-prediction) + (1 - true_output) * torch.exp(prediction)
--- src/DLL/MachineLearning/SupervisedLearning/Trees/GradientBoostingClassifier.py
+++ src/DLL/MachineLearning/SupervisedLearning/Trees/GradientBoostingClassifier.py
@@ -4,21 +4,36 @@ from .RegressionTree import RegressionTree
 from ....DeepLearning.Layers.Activations import Sigmoid, SoftMax
 from ....DeepLearning.Losses import bce, cce, exponential
 from ....Data.Preprocessing import OneHotEncoder
+from ....Exceptions import NotFittedError
 
 
 
 class GradientBoostingClassifier:
     """
-    GradientBoostingClassifier implements a classification algorithm fitting many consecutive :class:`DecisionTrees <DLL.MachineLearning.SupervisedLearning.Trees.DecisionTree>` to residuals of the model.
+    GradientBoostingClassifier implements a classification algorithm fitting many consecutive :class:`RegressionTrees <DLL.MachineLearning.SupervisedLearning.Trees.RegressionTree>` to residuals of the model.
 
     Args:
         n_trees (int, optional): The number of trees used for predicting. Defaults to 10. Must be a positive integer.
-        learning_rate (float, optional): The number multiplied to each additional trees residuals.
+        learning_rate (float, optional): The number multiplied to each additional trees residuals. Must be a real number in range (0, 1). Defaults to 0.5.
         max_depth (int, optional): The maximum depth of the tree. Defaults to 25. Must be a positive integer.
         min_samples_split (int, optional): The minimum required samples in a leaf to make a split. Defaults to 2. Must be a positive integer.
+        loss (string, optional): The loss function used in calculations of the residuals. Must be one of "log_loss" or "exponential". Defaults to "log_loss". "exponential" can only be used for binary classification.
+    Attributes:
+        n_features (int): The number of features. Available after fitting.
+        n_classes (int): The number of classes. 2 for binary classification. Available after fitting.
+        classes (torch.Tensor of shape (n_classes,)): The classes used as labels. Available after fitting. Contains integers [0, ..., n_classes - 1].
     """
-    def __init__(self, n_trees=10, learning_rate=0.1, max_depth=25, min_samples_split=2, loss="log_loss"):
-        # raise valueError if loss wrong
+    def __init__(self, n_trees=10, learning_rate=0.5, max_depth=25, min_samples_split=2, loss="log_loss"):
+        if not isinstance(n_trees, int) or n_trees < 1:
+            raise ValueError("n_trees must be a positive integer.")
+        if not isinstance(learning_rate, float) or learning_rate <= 0 or learning_rate >= 1:
+            raise ValueError("learning_rate must be a float in range (0, 1).")
+        if not isinstance(max_depth, int) or max_depth < 1:
+            raise ValueError("max_depth must be a positive integer.")
+        if not isinstance(min_samples_split, int) or min_samples_split < 1:
+            raise ValueError("min_samples_split must be a positive integer.")
+        if loss not in ["log_loss", "exponential"]:
+            raise ValueError('loss must be one of ["log_loss", "exponential"]')
         self.n_trees = n_trees
         self.learning_rate = learning_rate
         self.max_depth = max_depth
@@ -26,27 +41,49 @@ class GradientBoostingClassifier:
         self.trees = []
         self.loss_ = loss
     
-    def _get_activation_and_loss(self, n_classes):
-        self.n_classes = n_classes
+    def _get_activation_and_loss(self, classes):
+        self.classes = classes
+        self.n_classes = len(classes)
         if self.loss_ == "log_loss":
-            if n_classes == 2:
+            if self.n_classes == 2:
                 self.loss = bce()
                 self.activation = Sigmoid()
             else:
                 self.loss = cce()
                 self.activation = SoftMax()
         elif self.loss_ == "exponential":
-            if n_classes != 2:
+            if self.n_classes != 2:
                 raise ValueError("The exponential loss is only applicable in binary classification. Use log_loss for multiclass classification instead.")
             self.loss = exponential()
             self.activation = Sigmoid()
 
     def fit(self, X, y):
-        assert y.ndim == 1 or (y.ndim == 2 and y.shape[1] == 1), "y has to be a 1 dimensional vector."
-        assert set(torch.unique(y).numpy()) == {0, 1}, "The classes must be labelled 0 and 1."
-        self._get_activation_and_loss(len(torch.unique(y)), y)
+        """
+        Fits the GradientBoostingClassifier model to the input data by fitting trees too the errors made by previous trees.
 
-        y = y.squeeze().to(X.dtype)
+        Args:
+            X (torch.Tensor of shape (n_samples, n_features)): The input data, where each row is a sample and each column is a feature.
+            y (torch.Tensor of shape (n_samples,)): The labels corresponding to each sample. Every element must be in [0, ..., n_classes - 1].
+        Returns:
+            None
+        Raises:
+            TypeError: If the input matrix or the label vector is not a PyTorch tensor.
+            ValueError: If the input matrix or the label vector is not the correct shape or the label vector contains wrong values.
+        """
+        if not isinstance(X, torch.Tensor) or not isinstance(y, torch.Tensor):
+            raise TypeError("The input matrix and the label matrix must be a PyTorch tensor.")
+        if X.ndim != 2:
+            raise ValueError("The input matrix must be a 2 dimensional tensor.")
+        if y.ndim != 1 or y.shape[0] != X.shape[0]:
+            raise ValueError("The labels must be 1 dimensional with the same number of samples as the input data")
+        vals = torch.unique(y).numpy()
+        if set(vals) != {*range(len(vals))}:
+            raise ValueError("y must only contain the values in [0, ..., n_classes - 1].")
+        
+        self._get_activation_and_loss(torch.unique(y))
+        y = y.to(X.dtype)
+        self.n_features = X.shape[1]
+
         if self.n_classes == 2:
             self._binary_fit(X, y)
         else:
@@ -70,12 +107,13 @@ class GradientBoostingClassifier:
     
     def _multi_fit(self, X, y):
         encoder = OneHotEncoder()
+        encoder.fit(y)
         y = encoder.one_hot_encode(y)
         
         self.initial_log_odds = 0.0
         pred = torch.full(y.shape, self.initial_log_odds)
 
-        for class_index in self.n_classes:
+        for class_index in range(self.n_classes):
             class_trees = []
             for _ in range(self.n_trees):
                 prob = self.activation.forward(pred)
@@ -90,7 +128,28 @@ class GradientBoostingClassifier:
             self.trees.append(class_trees)
     
     def predict_proba(self, X):
-        assert hasattr(self, "initial_log_odds"), "GradientBoostingClassifier.fit(X, y) has to be called before predicting."
+        """
+        Applies the fitted GradientBoostingClassifier model to the input data, predicting the probabilities of each class.
+
+        Args:
+            X (torch.Tensor of shape (n_samples, n_features)): The input data to be classified.
+        Returns:
+            probabilities (torch.Tensor of shape (n_samples, n_classes) or for binary classification (n_samples,)): The predicted probabilities corresponding to each sample.
+        Raises:
+            NotFittedError: If the GradientBoostingClassifier model has not been fitted before predicting.
+            TypeError: If the input matrix is not a PyTorch tensor.
+            ValueError: If the input matrix is not the correct shape.
+        """
+        if not hasattr(self, "initial_log_odds"):
+            raise NotFittedError("GradientBoostingClassifier.fit() must be called before predicting.")
+        if not isinstance(X, torch.Tensor):
+            raise TypeError("The input matrix must be a PyTorch tensor.")
+        if X.ndim != 2 or X.shape[1] != self.n_features:
+            raise ValueError("The input matrix must be a 2 dimensional tensor with the same number of features as the fitted tensor.")
+
+        if self.n_classes > 2:
+            return self._multi_predict_proba(X)
+        
         pred = torch.full((X.shape[0],), self.initial_log_odds)
         
         for tree in self.trees:
@@ -100,6 +159,37 @@ class GradientBoostingClassifier:
         return self.activation.forward(pred)
 
     def predict(self, X):
-        assert hasattr(self, "initial_log_odds"), "GradientBoostingClassifier.fit(X, y) has to be called before predicting."
+        """
+        Applies the fitted GradientBoostingClassifier model to the input data, predicting the correct classes.
+
+        Args:
+            X (torch.Tensor of shape (n_samples, n_features)): The input data to be classified.
+        Returns:
+            labels (torch.Tensor of shape (n_samples,)): The predicted labels corresponding to each sample.
+        Raises:
+            NotFittedError: If the GradientBoostingClassifier model has not been fitted before predicting.
+            TypeError: If the input matrix is not a PyTorch tensor.
+            ValueError: If the input matrix is not the correct shape.
+        """
+        if not hasattr(self, "initial_log_odds"):
+            raise NotFittedError("GradientBoostingClassifier.fit() must be called before predicting.")
+        if not isinstance(X, torch.Tensor):
+            raise TypeError("The input matrix must be a PyTorch tensor.")
+        if X.ndim != 2 or X.shape[1] != self.n_features:
+            raise ValueError("The input matrix must be a 2 dimensional tensor with the same number of features as the fitted tensor.")
+        
         prob = self.predict_proba(X)
-        return (prob >= 0.5).to(torch.int32)
+        if self.n_classes == 2:
+            return (prob >= 0.5).to(torch.int32)
+        return self.classes[prob.argmax(dim=1)]
+    
+    def _multi_predict_proba(self, X):
+        pred = torch.full((X.shape[0], self.n_classes), self.initial_log_odds)
+
+        for i in range(self.n_classes):
+            class_trees = self.trees[i]
+            for tree in class_trees:
+                pred[:, i] += self.learning_rate * tree.predict(X)
+
+        return self.activation.forward(pred)
+
