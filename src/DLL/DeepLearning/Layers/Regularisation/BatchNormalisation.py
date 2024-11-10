Starting group normalisation refactor and bug fixing

src/DLL/DeepLearning/Layers/Regularisation/BatchNormalisation.py
@@ -1,19 +1,31 @@
 import torch
 import numpy as np
+
 from .BaseRegularisation import BaseRegularisation
 
 
 class BatchNorm(BaseRegularisation):
-    def __init__(self, output_shape=None, patience=0.9, **kwargs):
-        super().__init__(output_shape, **kwargs)
-        assert 0 < patience and patience < 1, "Patience must be strictly between 0 and 1"
+    """
+    The batch normalisation layer for neural networks.
+
+    Args:
+        patience (float, optional): The number deciding how fast the mean and variance in training. Must be strictly between 0 and 1. Defaults to 0.9.
+    """
+    def __init__(self, patience=0.9, **kwargs):
+        if patience <= 0 or 1 <= patience:
+            raise ValueError("patience must be strictly between 0 and 1.")
+        
+        super().__init__(**kwargs)
         self.patience = patience
         self.epsilon = 1e-6
         self.name = "Batch normalisation"
-        if self.output_shape is not None: self.initialise_layer(self.output_shape, self.data_type, self.device)
     
-    def initialise_layer(self, input_shape, data_type, device):
-        super().initialise_layer(input_shape, data_type, device)
+    def initialise_layer(self, **kwargs):
+        """
+        :meta private:
+        """
+
+        super().initialise_layer(**kwargs)
 
         self.gamma = torch.ones(self.output_shape, dtype=self.data_type, device=self.device)
         self.beta = torch.zeros(self.output_shape, dtype=self.data_type, device=self.device)
@@ -22,7 +34,30 @@ class BatchNorm(BaseRegularisation):
         self.nparams = 2 * np.prod(self.output_shape)
 
     def forward(self, input, training=False, **kwargs):
-        assert input.shape[0] > 1, "Batch size must be greater than one"
+        """
+        Normalises the input to have zero mean and one variance with the following equation:
+        
+        .. math::
+            y = \\gamma\\frac{x - \\mathcal{E}[x]}{\\sqrt{\\text{var}(x) + \\epsilon}} + \\beta,
+        
+        where :math:`x` is the input, :math:`\\mathcal{E}[x]` is the expected value or the mean accross the batch dimension, :math:`\\text{var}(x)` is the variance accross the variance accross the batch dimension, :math:`\\epsilon` is a small constant and :math:`\\gamma` and :math:`\\beta` are trainable parameters.
+
+        Args:
+            input (torch.Tensor of shape (batch_size, channels, ...)): The input to the layer. Must be a torch.Tensor of the spesified shape given by layer.input_shape.
+            training (bool, optional): The boolean flag deciding if the model is in training mode. Defaults to False.
+
+        Returns:
+            torch.Tensor: The output tensor after the normalisation with the same shape as the input.
+        """
+        if not isinstance(input, torch.Tensor):
+            raise TypeError("input must be a torch.Tensor.")
+        if input.shape[1:] != self.input_shape:
+            raise ValueError(f"input is not the same shape as the spesified input_shape ({input.shape[1:], self.input_shape}).")
+        if not isinstance(training, bool):
+            raise TypeError("training must be a boolean.")
+        if input.shape[0] <= 1:
+            raise ValueError("The batch size must be atleast 2.")
+        
         self.input = input
         if training:
             mean = torch.mean(input, axis=0)
@@ -38,6 +73,20 @@ class BatchNorm(BaseRegularisation):
         return self.output
     
     def backward(self, dCdy, **kwargs):
+        """
+        Calculates the gradient of the loss function with respect to the input of the layer. Also calculates the gradients of the loss function with respect to the model parameters.
+
+        Args:
+            dCdy (torch.Tensor of the same shape as returned from the forward method): The gradient given by the next layer.
+            
+        Returns:
+            torch.Tensor of shape (batch_size, channels, ...): The new gradient after backpropagation through the layer.
+        """
+        if not isinstance(dCdy, torch.Tensor):
+            raise TypeError("dCdy must be a torch.Tensor.")
+        if dCdy.shape[1:] != self.output.shape[1:]:
+            raise ValueError(f"dCdy is not the same shape as the spesified output_shape ({dCdy.shape[1:], self.output.shape[1:]}).")
+        
         batch_size = self.output.shape[0]
         dCdx_norm = dCdy * self.gamma
         dCdgamma = (dCdy * self.x_norm).sum(axis=0)
@@ -51,5 +100,7 @@ class BatchNorm(BaseRegularisation):
         return dCdx
     
     def get_parameters(self):
+        """
+        :meta private:
+        """
         return (self.gamma, self.beta)
-    

src/DLL/DeepLearning/Layers/Regularisation/GroupNormalisation.py
@@ -1,43 +1,74 @@
 import torch
+import numpy as np
+
 from .BaseRegularisation import BaseRegularisation
 
 
-"""
-Computes the group norm of a batch along axis=1
-
-input.shape = (batch_size, channels, *)
-output.shape = (batch_size, channels, *)
-"""
 class GroupNorm(BaseRegularisation):
-    def __init__(self, output_shape=None, num_groups=32, **kwargs):
-        super().__init__(output_shape, **kwargs)
-        self.kwargs = kwargs
+    """
+    The group normalisation layer for neural networks. Computes the group norm of a batch along axis=1
+
+    Args:
+        num_groups (int, optional): The number of groups used in the normalisation. Must be a positive integer. Defaults to 32. The number of channels must be evenly divisible by num_groups. If is set to 1, is identical to layer normalisation and if batch_size, is identical to the instance normalisation.
+    """
+    def __init__(self, num_groups=32, **kwargs):
+        if num_groups is not None and (not isinstance(num_groups, int) or num_groups <= 0):
+            raise ValueError("num_groups must be a positive integer.")
+
+        super().__init__(**kwargs)
         self.num_groups = num_groups
         self.epsilon = 1e-6
         self.name = "Group normalisation"
-        if self.output_shape is not None: self.initialise_layer(self.output_shape, self.data_type, self.device)
     
-    def initialise_layer(self, input_shape, data_type, device):
-        super().initialise_layer(input_shape, data_type, device)
-        assert self.output_shape[0] % self.num_groups == 0, "output_shape must be divisible by the number of groups"
-        assert self.output_shape[0] // self.num_groups > 1, "Number of elements in each group must be greater than 1"
+    def initialise_layer(self, **kwargs):
+        """
+        :meta private:
+        """
+
+        super().initialise_layer(**kwargs)
+
+        if self.output_shape[0] % self.num_groups != 0:
+            raise ValueError("The number of channels must be evenly divisible by num_groups.")
         
         self.gamma = torch.ones(self.output_shape, device=self.device, dtype=self.data_type)
         self.beta = torch.zeros(self.output_shape, device=self.device, dtype=self.data_type)
         self.nparams = 2 * self.output_shape[0]
 
     def forward(self, input, **kwargs):
+        """
+        Normalises the input to have zero mean and one variance accross self.num_groups groups accross the channel dimension with the following equation:
+        
+        .. math::
+            y = \\gamma\\frac{x - \\mathcal{E}[x]}{\\sqrt{\\text{var}(x) + \\epsilon}} + \\beta,
+        
+        where :math:`x` is the input, :math:`\\mathcal{E}[x]` is the expected value or the mean accross each group, :math:`\\text{var}(x)` is the variance accross the variance accross each group, :math:`\\epsilon` is a small constant and :math:`\\gamma` and :math:`\\beta` are trainable parameters.
+
+        Args:
+            input (torch.Tensor of shape (batch_size, channels, ...)): The input to the layer. Must be a torch.Tensor of the spesified shape given by layer.input_shape.
+            training (bool, optional): The boolean flag deciding if the model is in training mode. Defaults to False.
+            
+        Returns:
+            torch.Tensor: The output tensor after the normalisation with the same shape as the input.
+        """
+        if not isinstance(input, torch.Tensor):
+            raise TypeError("input must be a torch.Tensor.")
+        if input.shape[1:] != self.input_shape:
+            raise ValueError(f"input is not the same shape as the spesified input_shape ({input.shape[1:], self.input_shape}).")
+        print(np.prod(input.shape) // self.num_groups)
+        if np.prod(input.shape) // self.num_groups <= 1:
+            raise ValueError("The number of elements in each group must be greater than 1.")
+
         elements_per_group = input.shape[1] // self.num_groups
-        assert elements_per_group > 1, "There must be more than 1 element in a group"
         self.input = input
         batch_size = input.shape[0]
         self.input = input
         self.input_reshaped = self.input.view(batch_size, self.num_groups, elements_per_group, *input.shape[2:])
         mean = 1.0 / elements_per_group * self.input_reshaped.sum(2, keepdim=True)
+
         self.x_centered = self.input_reshaped - mean
         self.x_centered_squared = self.x_centered ** 2
-        # unbiased variance
-        self.var = 1.0 / (elements_per_group - 1) * self.x_centered_squared.sum(2, keepdim=True)
+        self.var = 1.0 / (elements_per_group - 1) * self.x_centered_squared.sum(2, keepdim=True)  # unbiased variance
+
         self.inv_std = (self.var + self.epsilon) ** -0.5
         self.x_norm = self.x_centered * self.inv_std
         self.x_reshaped = self.x_norm.view(self.input.shape)
@@ -45,6 +76,20 @@ class GroupNorm(BaseRegularisation):
         return self.output
     
     def backward(self, dCdy, **kwargs):
+        """
+        Calculates the gradient of the loss function with respect to the input of the layer. Also calculates the gradients of the loss function with respect to the model parameters.
+
+        Args:
+            dCdy (torch.Tensor of the same shape as returned from the forward method): The gradient given by the next layer.
+            
+        Returns:
+            torch.Tensor of shape (batch_size, channels, ...): The new gradient after backpropagation through the layer.
+        """
+        if not isinstance(dCdy, torch.Tensor):
+            raise TypeError("dCdy must be a torch.Tensor.")
+        if dCdy.shape[1:] != self.output.shape[1:]:
+            raise ValueError(f"dCdy is not the same shape as the spesified output_shape ({dCdy.shape[1:], self.output.shape[1:]}).")
+        
         batch_size = dCdy.shape[0]
         elements_per_group = self.output.shape[1] // self.num_groups
         dCdx_reshaped = dCdy * self.gamma
@@ -66,4 +111,7 @@ class GroupNorm(BaseRegularisation):
         return dCdx.view(self.output.shape)
 
     def get_parameters(self):
+        """
+        :meta private:
+        """
         return (self.gamma, self.beta)