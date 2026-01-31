c37a143994eef73548260458b6051ad18771736c
naapeli <aatu.selkee@gmail.com>
2026-01-31 21:51:31 +0200

    Added parallel training to the RandomForest with joblib

--- DLL/MachineLearning/SupervisedLearning/Trees/_RandomForest.py
+++ DLL/MachineLearning/SupervisedLearning/Trees/_RandomForest.py
@@ -1,5 +1,7 @@
 import torch
-from collections import Counter
+
+from joblib import Parallel, delayed
+import os
 
 from ._DecisionTree import DecisionTree
 from ....Exceptions import NotFittedError
@@ -13,18 +15,31 @@ class RandomForestClassifier:
         n_trees (int, optional): The number of trees used for predicting. Defaults to 10. Must be a positive integer.
         max_depth (int, optional): The maximum depth of the tree. Defaults to 10. Must be a positive integer.
         min_samples_split (int, optional): The minimum required samples in a leaf to make a split. Defaults to 2. Must be a positive integer.
+        n_jobs (int, optional): The number of parallel workers used to fit the model.
     """
-    def __init__(self, n_trees=10, max_depth=10, min_samples_split=2):
+    def __init__(self, n_trees=10, max_depth=10, min_samples_split=2, criterion="gini", ccp_alpha=0, n_jobs=-1):
         if not isinstance(n_trees, int) or n_trees < 1:
             raise ValueError("n_trees must be a positive integer.")
         if not isinstance(max_depth, int) or max_depth < 1:
             raise ValueError("max_depth must be a positive integer.")
         if not isinstance(min_samples_split, int) or min_samples_split < 1:
             raise ValueError("min_samples_split must be a positive integer.")
+        if not isinstance(n_jobs, int) or n_jobs <= -2 or n_jobs > os.cpu_count():
+            raise ValueError("n_jobs must be an integer between -1 and os.cpu_count()")
+        
         self.n_trees = n_trees
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
-        self.trees = [DecisionTree(max_depth=max_depth, min_samples_split=min_samples_split) for _ in range(n_trees)]
+        self.criterion = criterion
+        self.ccp_alpha = ccp_alpha
+        self.n_jobs = n_jobs 
+        self.trees = None
+
+    def _fit_single_tree(self, X, y):
+        X_boot, y_boot = self._bootstrap_sample(X, y)
+        tree = DecisionTree(max_depth=self.max_depth, min_samples_split=self.min_samples_split)
+        tree.fit(X_boot, y_boot)
+        return tree
 
     def fit(self, X, y):
         """
@@ -45,8 +60,8 @@ class RandomForestClassifier:
             raise ValueError("The input matrix must be a 2 dimensional tensor.")
         if y.ndim != 1 or y.shape[0] != X.shape[0]:
             raise ValueError("The labels must be 1 dimensional with the same number of samples as the input data")
-        for tree in self.trees:
-            tree.fit(*self._bootstrap_sample(X, y))
+        
+        self.trees = Parallel(n_jobs=self.n_jobs)(delayed(self._fit_single_tree)(X, y) for _ in range(self.n_trees))
 
     def predict(self, X):
         """
@@ -67,8 +82,11 @@ class RandomForestClassifier:
             raise TypeError("The input matrix must be a PyTorch tensor.")
         if X.ndim != 2 or X.shape[1] != self.trees[0].n_features:
             raise ValueError("The input matrix must be a 2 dimensional tensor with the same number of features as the fitted tensor.")
-        predictions = torch.stack([tree.predict(X) for tree in self.trees]).T
-        return torch.tensor([Counter(sample_prediction).most_common(1)[0][0] for sample_prediction in predictions])
+
+        tree_preds = Parallel(n_jobs=self.n_jobs)(delayed(tree.predict)(X) for tree in self.trees)
+        predictions = torch.stack(tree_preds).T
+        final_preds = torch.mode(predictions, dim=1).values
+        return final_preds
     
     def predict_proba(self, X):
         """
@@ -89,7 +107,9 @@ class RandomForestClassifier:
             raise TypeError("The input matrix must be a PyTorch tensor.")
         if X.ndim != 2 or X.shape[1] != self.trees[0].n_features:
             raise ValueError("The input matrix must be a 2 dimensional tensor with the same number of features as the fitted tensor.")
-        return torch.stack([tree.predict_proba(X) for tree in self.trees]).mean(dim=0)
+
+        tree_probs = Parallel(n_jobs=self.n_jobs)(delayed(tree.predict_proba)(X) for tree in self.trees)
+        return torch.stack(tree_probs).mean(dim=0)
 
     def _bootstrap_sample(self, X, y):
         indices = torch.randint(high=len(y), size=(len(y), 1)).flatten()
